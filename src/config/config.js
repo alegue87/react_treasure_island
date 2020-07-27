54caf41e91e8dd25c71aa1127a39c664e8522a33
alessio <alegue87@gmail.com>
2020-07-27 19:52:11 +0200

    chapter-28

--- src/config/config.js
+++ src/config/config.js
@@ -12,7 +12,7 @@ var GLOBAL = {
   API_MENU_URL: siteurl  + '/wp-json/menu/',
   API_DICTIONARY_URL: siteurl + '/wp-json/dictionary/',
 	//token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC93d3cud3B0ZXN0Lm1lIiwiaWF0IjoxNTk0MTI2NDQ4LCJuYmYiOjE1OTQxMjY0NDgsImV4cCI6MTU5NDczMTI0OCwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.au8Hq_AMO_lhqGYEkxN2jc-wmXw-bNVaUqDjlah6VZo"
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90cmVhc3VyZWlzbGFuZC5sb2NhbCIsImlhdCI6MTU5NTE2MTU3OCwibmJmIjoxNTk1MTYxNTc4LCJleHAiOjE1OTU3NjYzNzgsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.EJTZURBHy7q036uojs9N-tD5-G5wItQfHb5s7H0r8hE"
+  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90cmVhc3VyZWlzbGFuZC5sb2NhbCIsImlhdCI6MTU5NTc4NDQyMywibmJmIjoxNTk1Nzg0NDIzLCJleHAiOjE1OTYzODkyMjMsImRhdGEiOnsidXNlciI6eyJpZCI6IjEifX19.gx4rCyhYq4C-CgZgOAhr2AsKJzfJezMIVaYYf-wHs_w"
 }
 
 export default GLOBAL
